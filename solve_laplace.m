1e1986d0119cd8a82e8ec395fa5d4d60beae9895
nahometewolde <nahome.t@icloud.com>
2020-12-20 01:01:50 +0000

    Created function to solve laplace eqn

---
+++ solve_laplace.m
@@ -0,0 +1,7 @@
+function [psi,hist_values] = solve_laplace(init_psi,alpha, N_iter)
+% Solves laplaces equation via method of over-relaxation
+% I
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
