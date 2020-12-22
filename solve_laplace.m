9eca01ffd841af08b9c39b8c9ce1834a8f1f49bf
nahometewolde <nahome.t@icloud.com>
2020-12-22 02:20:58 +0000

    Started over-relaxation algorithm, iterates over each box and calculates the residues, then uses that in order to find a better approximation for psi.

--- solve_laplace.m
+++ solve_laplace.m
@@ -6,11 +6,28 @@ function [psi,hist_values] = solve_laplace(init_psi,alpha, N_iter)
 
 % Output:
 % * psi: 2D matrix of the value of \psi after (up to) N_iter iterations.
+%       represents an approximated solution to laplace's equation
 % * hist_values: (N_iter x 3) matrix that contains historical values of 3 points during % the iteration (1 in the upper half, 1 in the middle, and 1 in the lower half). %
 % Constraints:
 % * The boundaries of \psi are kept constant during the iterations.
 
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+%Gathering maximum values for i and j
+maxI = size(init_psi, 1);
+maxJ = size(init_psi, 2);
+
+% Will store the 
+hist_values = zeros(N_iter, 3);
+
+
+psi = init_psi; 
+for x=1:N_iter %
+    for i=2:maxI-1
+        disp('hahahha')
+        for j = maxJ-1:-1:2 %Iterates backwards
+            R = psi(i, j+1)+psi(i, j-1) + psi(i+1, j)+ psi(i-1, j);
+            psi = psi + alpha*R/4;
+        end
+    end
+ 
 end
 
